f73b47744dc0764d7518a6031d73e0d976425e4a
Mat <whizarm@protonmail.com>
2023-08-08 08:30:03 +0200

    feat: better game result display
    Style game win/lose results to make them look better

--- src/components/GameResult/GameResult.tsx
+++ src/components/GameResult/GameResult.tsx
@@ -1,17 +1,21 @@
 import { GameState } from 'types';
+import styles from './GameResult.module.scss';
 
 type GameResultProps = {
   gameState: GameState;
 };
 
 export const GameResult = ({ gameState }: GameResultProps) => {
-  if (gameState === 'WON') {
-    return "Congratulations! You've' won 🎉";
-  }
-
-  if (gameState === 'LOST') {
-    return "Oh no! You've lost 🥹";
-  }
-
-  return;
+  return (
+    <div
+      className={[styles.gameResult, gameState !== '' ? styles.show : ''].join(
+        ' ',
+      )}
+    >
+      <h3>
+        {gameState === 'WON' && "Congratulations! You've won 🎉"}
+        {gameState === 'LOST' && "Oh no! You've lost 🥹"}
+      </h3>
+    </div>
+  );
 };
