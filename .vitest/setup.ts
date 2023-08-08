9838fc09ed57b5a69bdd58372aab5490ef8c8a95
Mat <whizarm@protonmail.com>
2023-08-08 09:36:37 +0200

    test: fix tests after modal changes
    Render all components in tests with the same modal container as present in index.html

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -10,4 +10,11 @@ beforeEach(() => {
     toastPortalRoot.setAttribute('id', 'error-toast');
     document.body.appendChild(toastPortalRoot);
   }
+
+  let modalPortalRoot = document.getElementById('modal-container');
+  if (!modalPortalRoot) {
+    modalPortalRoot = document.createElement('div');
+    modalPortalRoot.setAttribute('id', 'modal-container');
+    document.body.appendChild(modalPortalRoot);
+  }
 });
