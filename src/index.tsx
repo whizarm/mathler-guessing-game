e1ef2678ac24d13cde9ffa73f7e06ce9f3887211
Mat <whizarm@protonmail.com>
2023-08-09 15:07:48 +0200

    style: add handwritten font
    Add The Barethos font for nice handwriting-like style in headers

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import 'assets/TheBarethos.ttf';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
