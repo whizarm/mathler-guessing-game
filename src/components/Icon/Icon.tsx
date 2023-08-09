b3c4f2f86809ce9f0cd146b586997ce0c6abdb46
Mat <whizarm@protonmail.com>
2023-08-09 15:09:40 +0200

    feat: add icon component
    Add Icon component serving svg and png icons

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,25 @@
+import deleteIcon from 'assets/delete.svg';
+import enterIcon from 'assets/enter.svg';
+import logoIcon from 'assets/numbers-questionmark.png';
+
+type IconProps = {
+  icon: 'enter' | 'delete' | 'logo';
+  height?: string;
+};
+
+const getIcon = (icon: IconProps['icon']) => {
+  switch (icon) {
+    case 'enter':
+      return enterIcon;
+    case 'delete':
+      return deleteIcon;
+    case 'logo':
+      return logoIcon;
+    default:
+      return;
+  }
+};
+
+export const Icon = ({ icon, height = '100%' }: IconProps) => (
+  <img src={getIcon(icon)} alt={icon} height={height} />
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from './Button/Button';
 export * from './ErrorToast/ErrorToast';
 export * from './GameResult/GameResult';
 export * from './Header/Header';
+export * from './Icon/Icon';
 export * from './InputPanel/InputPanel';
 export * from './Modal/Modal';
 export * from './Tile/Tile';
